b15c5b34c9e39bf372cbd05c514edda48d51c7dd
Henok <henoksolomongiz@gmail.com>
2022-05-09 10:14:07 +0300

    Add project files.

---
+++ TestApp/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace TestApp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TestApp.TestDB>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(TestApp.TestDB context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ TestApp/TestDB.cs
@@ -0,0 +1,35 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+
+namespace TestApp
+{
+    public class TestDB : DbContext
+    {
+        // Your context has been configured to use a 'TestDB' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'TestApp.TestDB' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'TestDB' 
+        // connection string in the application configuration file.
+        public TestDB()
+            : base("name=TestDB")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        public virtual DbSet<UserEntity> Users { get; set; }
+    }
+
+    public class UserEntity
+    {
+        public string EmailId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; } 
+        public string DOB { get; set; }
+        public string Address { get; set; }
+    }
+}
